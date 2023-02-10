0fb020144efd8851f4dedf73f472378c1b9d1fb7
danieldeveloper <danielfelipedeveloper@gmail.com>
2023-02-10 09:59:13 -0400

    feat: Switching Pages

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,11 +5,12 @@ import { RiAddLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
-import { useEffect } from "react";
 import { useUsers } from "../../services/hooks/useUsers";
+import { useState } from "react";
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useUsers();
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isFetching, error } = useUsers(page);
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -71,7 +72,7 @@ export default function UserList() {
                 </Thead>
 
                 <Tbody>
-                  {data.map(user => (
+                  {data.users.map(user => (
                     <Tr key={user.id}>
                       <Td px={["4", "4", "6"]}>
                         <Checkbox colorScheme="pink" />
@@ -89,9 +90,9 @@ export default function UserList() {
               </Table>
 
               <Pagination 
-                totalCountOfRegisters={200}
-                currentPage={8}
-                onPageChange={() => {}}
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
               />
             </>
           )}
